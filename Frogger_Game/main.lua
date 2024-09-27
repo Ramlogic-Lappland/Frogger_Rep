dbf632e0881a3920f0024424569354661d6467af
Lappland <b4d4ssg04t@gmail.com>
2024-09-27 12:02:09 -0300

    Load frog png

--- Frogger_Game/main.lua
+++ Frogger_Game/main.lua
@@ -1,12 +1,19 @@
 function love.load()
+frogX = 100
+frogY = 100
+frogSpeed = 100
+
+frogImage = love.graphics.newImage("res/frogx2.png")
+
 
 end
 
-function love.update()
+function love.update(dt)
 
 end
 
 function love.draw()
+    love.graphics.draw(frogImage, frogX, frogY)
     love.graphics.print("Hello World", 600, 500)
 end
 
